520b6451a35dfdc7d901751d4ecb9215c2e3948e
devwatts <devwatts.1901010026@krmangalam.edu.in>
2021-06-03 18:13:10 +0530

    fixed

--- app.js
+++ app.js
@@ -98,7 +98,15 @@ try {
     (async () => {
 
         const browser = await puppeteer.launch({
-            headless: true
+            headless: true,
+            args:[
+                '--ignore-certificate-errors',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-accelerated-2d-canvas',
+                '--disable-gpu'
+              ]
+            
         });
 
         await pintresturlscrape(browser);
